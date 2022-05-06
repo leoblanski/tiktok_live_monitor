c350e19233a909494ef08cfe1cfefa22907bdbd7
Leonardo Blanski <leo_blanski@hotmail.com>
2022-05-05 23:13:38 -0300

    Alterações iniciais para vue

---
+++ tiktok_monitor/resources/js/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container-lg">
+     <h1>tesaate</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  setup: () => ({
+    devName: "Jorge Luís Gregório",
+  }),
+};
+</script>
+
+<style scoped>
+/* CSS Here! */
+</style>
---
+++ tiktok_monitor/resources/views/panel/panel.php
@@ -0,0 +1,8 @@
+<body>
+    <div id="app">
+        <Home />
+    </div>
+    <script src="<?= asset('js/app.js') ?>"></script>
+
+    
+</body>
